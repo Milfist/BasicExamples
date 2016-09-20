48bae6e2489d030f05c1bc72e7b39971c71dcfa6
Milfist <milfist@gmail.com>
2016-09-20 20:52:03 +0200

    Basic examples for Java
    Using static

---
+++ src/com/milfist/example/Area.java
@@ -0,0 +1,21 @@
+package com.milfist.example;
+
+public class Area {
+	
+	public static double quadrateStatic(double quadrateSide){
+		return Math.pow(quadrateSide, 2);
+	}
+	
+	public static double rectangleStatic(double rectangleSide1, double rectangleSide2) {
+		return rectangleSide1 * rectangleSide2;
+	}
+	
+	public double quadrate(double quadrateSide){
+		return Math.pow(quadrateSide, 2);
+	}
+	
+	public double rectangle(double rectangleSide1, double rectangleSide2) {
+		return rectangleSide1 * rectangleSide2;
+	}
+	
+}
---
+++ src/com/milfist/example/StaticExamples.java
@@ -0,0 +1,44 @@
+package com.milfist.example;
+
+/**
+ * Examples of using static. Attributes, methods and classes.
+ * 
+ * @author Mik
+ *
+ */
+public class StaticExamples {
+	
+	private static double rectangleSide1 = 5;
+	private static double num1 = 3;
+	private static double num2 = 9;
+
+	private static double quadrateSide = 6;
+	
+	public static void main (String arg[]) {		
+		
+		System.out.println("Static: El area de un cuadrado cuyo lado es " + quadrateSide + " : " + Area.quadrateStatic(quadrateSide));
+		System.out.println("Static: El area de un rectangulo cuyos lados son " + rectangleSide1 + " y " + rectangleSide2(num1, num2) + " es " + Area.rectangleStatic(rectangleSide1, rectangleSide2(num1, num2)));
+		
+		Area area = new Area();
+//Warning		area.quadrateStatic(quadrateSide); //The static method quadrateStatic(double) from the type Area should be accessed in a static way
+		
+		System.out.println("Non static: El area de un cuadrado cuyo lado es " + quadrateSide + " : " + area.quadrate(quadrateSide));
+		System.out.println("Non static: El area de un rectangulo cuyos lados son " + rectangleSide1 + " y " + rectangleSide2(num1, num2) + " es " + area.rectangle(rectangleSide1, rectangleSide2(num1, num2)));
+
+		Perimeter.quadrate();
+		
+		
+	}
+	
+	public static double rectangleSide2(double num1, double num2) {
+		return num1 + num2;		
+	}
+	
+	public static class Perimeter {
+        public static void quadrate(){
+            System.out.println("El perimetro de un cuadrado cuyo lado es " + (quadrateSide * 4));
+        }
+        
+    }
+	
+}
